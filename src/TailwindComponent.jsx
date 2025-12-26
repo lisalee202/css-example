ab7c61f39a128e7bf01380d51804c5d79440457c
lisalee <lisalee@lisaleeui-MacBookAir.local>
2025-12-26 12:56:15 +0900

    deploy correct project

---
+++ src/TailwindComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function TailwindComponent() {
+  return <div> 
+    <h1 className="text-2xl">Hello!</h1>
+    <button className="bg-yellow-500 rounded-xl px-2 ">Button</button>
+  </div>;
+}
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+
+//1
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+//2
+// module.exports = {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "./",
+  plugins: [react(), tailwindcss()],
+})
